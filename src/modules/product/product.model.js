b367e40a1d96777f1117d1c1d68e31780108aca9
Arian <arian.koochak@protonmail.com>
2024-11-10 20:53:07 +0330

    add unique product name

--- src/modules/product/product.model.js
+++ src/modules/product/product.model.js
@@ -2,7 +2,7 @@ const {Schema, model} = require('mongoose')
 
 const productSchema = new Schema(
     {
-        Name: { type: String, required: true, trim: true ,minLength : 2},
+        Name: { type: String, required: true, trim: true ,minLength : 2,unique : true},
         CategoryName: { type: String, required: true, trim: true ,minLength : 2,},
         Price: { type: String, required: true, trim: true ,minLength : 2,},
     },
