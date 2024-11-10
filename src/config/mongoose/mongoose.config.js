19f5f97aa3a866cede608035d15c559c55b27540
Arian <arian.koochak@protonmail.com>
2024-11-10 13:19:37 +0330

    fix database name

--- src/config/mongoose/mongoose.config.js
+++ src/config/mongoose/mongoose.config.js
@@ -1,7 +1,7 @@
 const { default: mongoose } = require("mongoose");
 const chalk = require("chalk");
 
-const DB_URL = "mongodb://127.0.0.1:27017/shop-project";
+const DB_URL = "mongodb://127.0.0.1:27017/categoryManagement";
 mongoose.set("strictQuery", true);
 mongoose.connect(DB_URL);
 
